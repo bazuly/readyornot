4d520f4b04ca384b6012fdbedca7413908d37b1d
Sereja <serejka50@gmail.com>
2024-02-08 20:42:39 +0300

    password recovery done 08.02

--- grandoproject/grandoproject/settings.py
+++ grandoproject/grandoproject/settings.py
@@ -122,3 +122,16 @@ AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
     "users.auth.EmailAuthBackend"
 ]
+
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.yandex.ru"
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "grandoemail@yandex.ru"
+EMAIL_HOST_PASSWORD = 'ohxulxwovxglolfm'
+EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
